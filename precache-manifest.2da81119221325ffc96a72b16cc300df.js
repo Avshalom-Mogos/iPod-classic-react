6280566099f21e326808dc74e2e4c7a61b7badec
Avshalom Mogos <avshalomogos@gmail.com>
2020-05-13 16:11:19 +0300

    Updates

---
+++ precache-manifest.2da81119221325ffc96a72b16cc300df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0be2f9a9a781d20561536102730861",
+    "url": "/iPod-classic-react/index.html"
+  },
+  {
+    "revision": "31498c217da520b68f9d",
+    "url": "/iPod-classic-react/static/css/main.bdf81282.chunk.css"
+  },
+  {
+    "revision": "81f961d2d42f0070f972",
+    "url": "/iPod-classic-react/static/js/2.b461fd94.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/iPod-classic-react/static/js/2.b461fd94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31498c217da520b68f9d",
+    "url": "/iPod-classic-react/static/js/main.b1a22650.chunk.js"
+  },
+  {
+    "revision": "3662fc4c0d4def5da9cf",
+    "url": "/iPod-classic-react/static/js/runtime-main.bced8ddc.js"
+  }
+]);
